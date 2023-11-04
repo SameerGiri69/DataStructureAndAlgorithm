0f2df300b62a4b1a40d5e2d8fe69bab4dec280fd
SameerGiri69 <sameergiri153000@gmail.com>
2023-11-04 11:01:34 +0545

    lambda expression

---
+++ MYPROJECT101/Lambda expression/Lambda.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYPROJECT101.Lambda_expression
+{
+    public class Lambda
+    {
+        public int Age { get; set ; }
+        public string Name { get; set; }
+
+       
+
+        public static List<Lambda> People = new List<Lambda>()
+        {
+            new Lambda { Age = 24, Name = "Sameer" },
+            new Lambda { Age = 19, Name = "Milan"},
+            new Lambda { Age = 69, Name = "HogRidaa"}
+        };
+        public static void AddPeople(int age, string name)
+        {
+            People.Add(new Lambda { Age = age, Name = name });
+        }
+
+        public static void DisplayPeople()
+        {
+            var sortByAge = People.OrderBy(p => p.Age);
+            foreach (var p in sortByAge)
+            {
+              //  Console.WriteLine();
+
+                Console.WriteLine(p.Age.ToString() + p.Name.ToString()) ;
+            }
+        }
+        
+    }
+}
